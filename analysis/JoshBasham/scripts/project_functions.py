e6b79e3867981931ab561b2f5d9728f0e0a193b9
JoshBasham <joshbasham34@gmail.com>
2021-08-02 16:59:09 -0700

    Created scripts dir and p_f.py

---
+++ analysis/JoshBasham/scripts/project_functions.py
@@ -0,0 +1,10 @@
+def load_and_process(path):
+    averageWeeks = 52.1429 # average weeks in a year
+    dn = pd.read_csv(path)
+    dn = (
+        pd.DataFrame(dn)
+       .drop(columns=['cg','cl','E-N'])
+      # .insert(loc = 0, column='Average-Hourly', value =round(dn['Salary'] / (dn['Hours-per-week']*averageWeeks)))       
+    )
+    dn['Average-Hourly'] = round((df2['Salary'] / (df2['Hours-per-week']*averageWeeks)),2)
+    return dn
